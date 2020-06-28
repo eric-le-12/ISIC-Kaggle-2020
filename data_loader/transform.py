ae6f280a652904a894f945aa0dcccf6de60f0c28
Hieu Le <lexuanhieu.bme@gmail.com>
2020-06-28 19:32:31 +0700

    first training version update transform

--- data_loader/transform.py
+++ data_loader/transform.py
@@ -1,17 +1,18 @@
 from torchvision import transforms
 from torchtoolbox.transform.cutout import Cutout_PIL as Cutout
 # define augmentation methods for training and validation/test set
+from albumentations import *
+from albumentations.pytorch import ToTensor
 
 train_transform = transforms.Compose(
-    [
-        transforms.Resize((224, 224)),
-        transforms.RandomHorizontalFlip(p=0.5),
-        transforms.RandomPerspective(),
-        transforms.RandomVerticalFlip(),
-        transforms.ToTensor(),
+    [  Blur(p=0.5,blur_limit=3)
+        VerticalFlip(p=.5),
+       HorizontalFlip(p=.5),
+       HueSaturationValue(),
+        ToTensor(),
     ]
 )
 
 val_transform = transforms.Compose(
-    [transforms.Resize((128, 128)), transforms.ToTensor(),]
+    [transforms.ToTensor()]
 )
