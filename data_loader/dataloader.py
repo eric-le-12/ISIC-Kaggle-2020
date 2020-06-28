d6b62b966c044bafb663c82dd59bd13b40bc9bca
Hieu Le <lexuanhieu.bme@gmail.com>
2020-06-28 19:57:59 +0700

    add packages

--- data_loader/dataloader.py
+++ data_loader/dataloader.py
@@ -4,6 +4,7 @@ from PIL import Image
 import os
 from sklearn.model_selection import train_test_split
 import cv2
+from matplotlib import pyplot as plt
 
 # define a data class
 class ClassificationDataset:
@@ -23,11 +24,15 @@ class ClassificationDataset:
         self.transform = transform
 
     def __getitem__(self, idx):
-        img = cv2.imread(os.path.join(self.data_path, self.data.iloc[idx, 0]))
-        label = self.data.iloc[idx, -1]
+        img = plt.imread(os.path.join(self.data_path, str(self.data.iloc[idx, 0])))
+        img = Image.fromarray(img).convert('RGB')
+        name = str(self.data.iloc[idx, 0])
+
         if self.transform is not None:
             img = self.transform(img)
-        img = img['image']
+        img = np.transpose(img, (2, 0, 1)).astype(np.float32)
+        label = self.data.iloc[idx, -1]
+
         return img, label
 
     def __len__(self):
@@ -52,11 +57,12 @@ class TestDataset:
 
     def __getitem__(self, idx):
         img = Image.open(os.path.join(self.data_path, str(self.data.iloc[idx, 0])))
+
         name = str(self.data.iloc[idx, 0])
 
         if self.transform is not None:
             img = self.transform(img)
-        
+
         return img,name
 
     def __len__(self):
